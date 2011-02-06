e1cf26e98e70a3583b49d33fc19ef4cb7b200317
Ionut Staicu <ionutzb@gmail.com>
2011-02-06 08:07:21 +0200

    added more conditional comments on html; revised the css with new/improved stuff

--- basefile.php
+++ basefile.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js msie ie6 lte9 lte7"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js msie ie7 lte9 lte7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js msie ie8 lte9 lte8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js msie ie9 lte9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>template file</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>base.project</title>
 		
 		<!-- html5 elements for ie<9 -->
 		<!--[if lte IE 8 ]> <script type="text/javascript" charset="utf-8"> /* <![CDATA[ */ var htmlForIe = ["abbr" ,"article" ,"aside" ,"audio" ,"canvas" ,"details" ,"figcaption" ,"figure" ,"footer" ,"header" ,"hgroup" ,"mark" ,"meter" ,"nav" ,"output" ,"progress" ,"section" ,"summary" ,"time" ,"video"], htmlForIeLen = htmlForIe.length; for(i=0;i<htmlForIeLen;i++){ document.createElement(htmlForIe[i]); } /* ]]> */ </script> <![endif]-->
@@ -26,6 +25,10 @@
 				})(document, 'script');
 			/* ]]> */
 		</script>
+<!--
+	  <link rel="shortcut icon" href="favicon.ico">
+		<link rel="apple-touch-icon" href="apple-touch-icon.png">
+-->
 	</head>
 	<body>
 		<div id="wrap">
