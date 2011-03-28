36c2945f011263ab9faa04c6d3a305fc7e425f58
Staicu Ionut <github@iamntz.com>
2011-03-28 17:24:43 +0300

    added sql queries for wp; sidebar cleaned;removed generator

--- wp-base/footer.php
+++ wp-base/footer.php
@@ -1,7 +1,13 @@
-<?php wp_footer(); ?>
+<?php global $ntz; wp_footer(); ?>
 
 <!--[if lte IE 8]>
 	<script src="<?php echo PATH;?>/js/lib/selectivizr.js" type="text/javascript" charset="utf-8"></script>
 <![endif]-->
+<?php if(is_user_logged_in() && current_user_can('update_core')) { ?> 
+	<script>
+		console.log('<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds.');
+	/* ]]> */
+	</script>
+<?php } ?>
 </body>
 </html>
