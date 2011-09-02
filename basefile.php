aa08c2102c9400b2524a1673e70a378c4fa5302b
Staicu Ionut <github@iamntz.com>
2011-09-02 12:35:26 +0300

    added ios fullscreen meta; removed some funky whitespaces

--- basefile.php
+++ basefile.php
@@ -5,16 +5,23 @@
 <!--[if IE 9 ]>    <html lang="en" class="no-js msie ie9 lte9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 	<head>
+		<title>base.project</title>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<title>base.project</title>
-		
+		<!-- uncomment next lines for native-looking app on ios devices -->
+		<!-- 
+			<meta name="apple-mobile-web-app-capable" content="yes"> 
+			<link rel="apple-touch-icon" href="apple-touch-icon.png">
+		-->
+
+		<!-- <link rel="shortcut icon" href="favicon.ico"> -->
+
 		<!-- html5 elements for ie<9 -->
 		<!--[if lte IE 8 ]> <script type="text/javascript">var htmlForIe = ["abbr" ,"article" ,"aside" ,"audio" ,"canvas" ,"details" ,"figcaption" ,"figure" ,"footer" ,"header" ,"hgroup" ,"mark" ,"meter" ,"nav" ,"output" ,"progress" ,"section" ,"summary" ,"time" ,"video"], htmlForIeLen = htmlForIe.length; for(i=0;i<htmlForIeLen;i++){ document.createElement(htmlForIe[i]); }</script> <![endif]-->
-		
+
 		<link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" />
-		
 		<script type="text/javascript">
+				// change UA-XXXXX-X to your g.analytics code
 				var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview'], ['_trackPageLoadTime']];
 				(function(d, t) {
 					var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
@@ -23,22 +30,17 @@
 					s.parentNode.insertBefore(g, s);
 				})(document, 'script');
 		</script>
-<!--
-	  <link rel="shortcut icon" href="favicon.ico">
-		<link rel="apple-touch-icon" href="apple-touch-icon.png">
--->
 	</head>
 	<body>
 		<div id="wrap">
-			
+
 		</div><!--/wrap-->
-	  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 		<script>!window.jQuery && document.write(unescape('%3Cscript src="js/lib/jquery-latest.min.js"%3E%3C/script%3E'))</script>
 
 		<!--[if lte IE 8]>
 			<script src="js/lib/selectivizr.js" type="text/javascript""></script>
 		<![endif]-->
-  
 		<script src="js/script.js" type="text/javascript"></script>
 	</body>
 </html>
