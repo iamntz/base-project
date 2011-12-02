65b2ec1699ca7ad42af6c48974f8c0a5b1d6af30
Staicu Ionut <github@iamntz.com>
2011-12-02 11:56:18 +0200

    Added some iphone related meta tags

--- basefile.php
+++ basefile.php
@@ -12,8 +12,12 @@
     <!-- uncomment next lines for native-looking app on ios devices -->
     <!-- 
       <meta name="apple-mobile-web-app-capable" content="yes"> 
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       <link rel="apple-touch-icon" href="apple-touch-icon.png">
     -->
+
+    <!-- very bad idea to have this enabled on all sites! -->
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" /> -->
 
     <!-- <link rel="shortcut icon" href="favicon.ico"> -->
 
