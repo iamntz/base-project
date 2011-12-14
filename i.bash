8e4d091a4741bb52f504c291f0752b7f461feff2
U-Ionut-PC\Ionut <Ionut@Ionut-PC.(none)>
2011-12-14 12:54:26 +0200

    fixed another bug on bash file; added curl instead of wget; removed git branches

--- i.bash
+++ i.bash
@@ -3,64 +3,56 @@
 read -p "Is WordPress? " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	wget http://wordpress.org/latest.tar.gz
-	tar --strip-components=1 -zxf latest.tar.gz
-	rm latest.tar.gz
-	
+curl -O http://wordpress.org/latest.tar.gz
+tar --strip-components=1 -zxf latest.tar.gz
+rm latest.tar.gz
 
-	echo "Theme name: "
-	read theme_name
-	cd themes/
-	mkdir $theme_name
-	cd $theme_name
-	mkdir includes
-	mkdir plugins
-	
-	cp -r ~/baseproject/* .
-	cp -r ~/baseproject//.[a-zA-Z0-9]* .
-	
-	cd js/lib/
-	  wget http://code.jquery.com/jquery-latest.min.js
+echo "Theme name: "
+read theme_name
+cd wp-content/themes/
+mkdir $theme_name
+cd $theme_name
+mkdir includes
+mkdir plugins
 
-	
-	cp -r ~/baseproject/wp-base/* .
-	
-	touch index.php
-	touch style.css
-	touch header.php
-	touch footer.php
-	
-	rm -rf .git
-	rm i.bash
-	rm README
-	rm -rf wp-base
+cp -r ~/baseproject/* .
+cp -r ~/baseproject//.[a-zA-Z0-9]* .
 
-	
+cd js/lib/
+  curl -O http://code.jquery.com/jquery-latest.min.js
+cd ../..
+
+cp -r ~/baseproject/wp-base/* .
+
+touch index.php
+touch style.css
+touch header.php
+touch footer.php
+
+rm -rf .git
+rm i.bash
+rm README
+rm -rf wp-base
+
 else
-	cp -r ~/baseproject/* .
-	cp -r ~/baseproject//.[a-zA-Z0-9]* .
-	
-	rm -rf .git
-	rm i.bash
-	rm README
-	
-	rm functions.php
-	rm -rf wp-base
-	
-	cd js/lib/
-	  wget http://code.jquery.com/jquery-latest.min.js
-	cd ../..
+cp -r ~/baseproject/* .
+cp -r ~/baseproject//.[a-zA-Z0-9]* .
+
+rm -rf .git
+rm i.bash
+rm README
+
+rm functions.php
+rm -rf wp-base
+
+cd js/lib/
+  curl -O http://code.jquery.com/jquery-latest.min.js
+cd ../..
 fi
 
 
 git init
 git add .
 git commit -a -m 'Initial Commit'
-
-git branch hotfix
-git branch release
-git branch develop
-
-
 
 echo 'Done! Enjoy!'
