a8b031edc2ec10850fc1933ea50f1d4ffd45dd7c
Staicu Ionut <github@iamntz.com>
2012-01-15 10:47:39 +0200

    js is a little more oop

--- js/script.js
+++ js/script.js
@@ -1,38 +1,41 @@
+var _Ntz = function(){
+  
+  this.init();
+};
+
+_Ntz.prototype = {
+  init : function(){
+    var $t = this;
+    $t.enablePlaceholderSupport();
+  }, // init
+  enablePlaceholderSupport : function(wrap){
+    var $                  = jQuery,
+        wrap               = wrap || 'body',
+        fakeInput          = document.createElement("input"),
+        placeHolderSupport = ("placeholder" in fakeInput),
+        clearValue         = function (e) { if ($(e).val() === $(e).data('placeholder')) { $(e).val(''); } };
+    if (!placeHolderSupport) {
+      $('input[placeholder]', wrap).each(function(){
+        var searchField  = $(this),
+            originalText = searchField.attr('placeholder'),
+            val          = $.trim(this.value);
+        if(typeof searchField.data('placeholder') !== 'undefined') { return; }
+        searchField.data('placeholder', originalText);
+        if(val == '') { $(this).val(originalText).addClass("placeholder"); }
+        searchField.bind("focus.ntz_placeholder", function () { if(this.value == $(this).data('placeholder')){ $(this).val('').removeClass('placeholder'); } }).bind("blur.ntz_placeholder", function () {
+          if (this.value.length === 0) {
+            $(this).val(originalText).addClass("placeholder");
+          }
+        });
+      });
+      $("form").bind("submit.ntz_placeholder", function () { $('input[placeholder]', this).each(function(){ clearValue(this); }); });
+      $(window).bind("unload.ntz_placeholder", function () { clearValue($('input[placeholder]', this)); });
+    }
+  }// enablePlaceholderSupport
+};
+
 jQuery(document).ready(function($){
   $('html').removeClass('no-js');
-  enablePlaceholderSupport();
+  var _ntz = new _Ntz();
 });
 
-
-
-
-function enablePlaceholderSupport(wrap){
-  var $ = jQuery,
-      wrap = wrap || 'body',
-      fakeInput = document.createElement("input"),
-      placeHolderSupport = ("placeholder" in fakeInput),
-      clearValue = function (e) { if ($(e).val() === $(e).data('placeholder')) { $(e).val(''); } };
-
-  if (!placeHolderSupport) {
-    $('input[placeholder]', wrap).each(function(){
-
-      var searchField = $(this),
-          originalText = searchField.attr('placeholder'),
-          val = $.trim(this.value);
-      if(typeof searchField.data('placeholder') !== 'undefined') { return; }
-
-      searchField.data('placeholder', originalText);
-      if(val == '') { $(this).val(originalText).addClass("placeholder"); }
-
-      searchField.bind("focus.ntz_placeholder", function () { if(this.value == $(this).data('placeholder')){ $(this).val('').removeClass('placeholder'); } }).bind("blur.ntz_placeholder", function () {
-        if (this.value.length === 0) {
-          $(this).val(originalText).addClass("placeholder");
-        }
-      });
-    });
-
-    $("form").bind("submit.ntz_placeholder", function () { $('input[placeholder]', this).each(function(){ clearValue(this); }); });
-
-    $(window).bind("unload.ntz_placeholder", function () { clearValue($('input[placeholder]', this)); });
-  }
-}; // enablePlaceholderSupport
