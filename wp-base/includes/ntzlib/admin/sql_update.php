df3905a101288603d11ae643c07b8dba43444b0e
Staicu Ionut <github@iamntz.com>
2012-04-03 11:21:12 +0300

    cleaned useless craps from sql

--- wp-base/includes/ntzlib/admin/sql_update.php
+++ wp-base/includes/ntzlib/admin/sql_update.php
@@ -38,39 +38,9 @@ if($ntz_db_version != $ntz_db_current_version){
     }
   }
 
-  $sql_tables[] = "CREATE TABLE $wpdb->hotel_room_types (
+  $sql_tables[] = "CREATE TABLE $wpdb->foo (
       `id` INT NOT NULL AUTO_INCREMENT,
-      `added_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-      `name` VARCHAR(255) NOT NULL ,
-      `description` TEXT NOT NULL,
-      `lang` VARCHAR(10) NOT NULL DEFAULT 'en',
-      `translation_of` INT NOT NULL,
-      UNIQUE KEY id (id)
-    ) $charset_collate;";
-
-  $sql_tables[] = "CREATE TABLE $wpdb->hotel_room_relationship (
-      `id` INT NOT NULL AUTO_INCREMENT,
-      `hotel_id` INT NOT NULL,
-      `translation_of_id` INT NOT NULL,
-      `room_type_id` INT NOT NULL,
-      `room_price_id` INT NOT NULL,
-      `room_dates` INT NOT NULL,
-      UNIQUE KEY id (id)
-    ) $charset_collate;";
-
-  $sql_tables[] = "CREATE TABLE $wpdb->hotel_room_prices (
-      `id` INT NOT NULL AUTO_INCREMENT,
-      `name` VARCHAR(255) NOT NULL,
-      `base_price` VARCHAR(255) NOT NULL,
-        `base_start_date` DATETIME NOT NULL,
-        `base_end_date` DATETIME NOT NULL,
-      `price_for_adult` VARCHAR(255) NOT NULL,
-        `adult_start_date` DATETIME NOT NULL,
-        `adult_end_date` DATETIME NOT NULL,
-      `price_for_child` VARCHAR(255) NOT NULL,
-        `child_start_date` DATETIME NOT NULL,
-        `child_end_date` DATETIME NOT NULL,
-      `availability` INT DEFAULT '10',
+      `bar` VARCHAR(255) NOT NULL ,
       UNIQUE KEY id (id)
     ) $charset_collate;";
 
